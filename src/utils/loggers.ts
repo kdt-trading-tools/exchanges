aed38691d40ffe87d78647cf4b77953a239cb42d
Diep Dang <khangdt310722@gmail.com>
2023-12-10 00:18:27 +0700

    refactor(logger): forward console.trace to console.debug

--- src/utils/loggers.ts
+++ src/utils/loggers.ts
@@ -1,6 +1,6 @@
 import type { Logger } from '../types'
 
-export const consoleLogger: Logger = console
+export const consoleLogger: Logger = { ...console, trace: (...args: any[]) => console.debug(...args) }
 
 export const silentLogger: Logger = {
     debug: () => void 0,
